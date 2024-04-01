ce6b772271e9596f023a1235093d09d49b7f7e21
TacosConChelas <adriherandezor@gmail.com>
2024-04-01 14:11:40 -0600

    cambios de la clase 6

--- Clase04_05/prueba.js
+++ Clase04_05/prueba.js
@@ -1,1 +1,17 @@
 console.log("He esperado tanto tiempo para volver a hacer esto de nuevo");
+let a = 7;
+let b = 7.8;
+let c = true;
+let d = null;
+let f = "hola mundo";
+c = "Hola"; 
+
+c = 4.4;
+
+c = null;
+
+console.log(1 + undefined);
+/**
+ *  0 != null
+    System.out.print()
+ */
